117bcaa7b6693a1bb31dc5023739b60d79b76aaa
Krystal Flores <krystal.flores@gmail.com>
2018-12-17 22:13:58 -0800

    Search works

--- routes/sightings.js
+++ routes/sightings.js
@@ -29,20 +29,57 @@ router.post('/', function(req, res, next) {
 //   only supported value is `num_bears`)
 
 /* GET /sighting/search */
-router.get('/search', function(req, res) {
+router.get('/search', function(req, res, next) {
   var query = req.query;
-  console.log(query);
 
+  //if there is a start date
   if (query.start_date) {
-    query.created_at = new Date(query.start_date);
+    // var start_date = new Date(start_date);
+    var start_date = new Date(query.start_date);
+
+    console.log('\tGETTING THE YEAR, MONTH, DATE');
+    var year = start_date.getFullYear();
+    console.log(`year: ${year}`);
+
+    var month = start_date.getMonth();
+    console.log(`month: ${month}`);
+
+    var date = start_date.getDate();
+    console.log(`date: ${date}`);
+
+    console.log(year);
+    query.created_at = {
+      $gt: new Date(year, month, date),
+    }
+    delete query.start_date;
   }
 
+  //if there is a end date
+  if (query.end_date) {
+    // var start_date = new Date(start_date);
+    var end_date = new Date(query.end_date);
 
-  Sighting.find(query, {
-    sort: {
-        num_bears: 1 //Sort by Date Added DESC
+    console.log('\tGETTING THE YEAR, MONTH, DATE');
+    var year = end_date.getFullYear();
+    console.log(`year: ${year}`);
+
+    var month = end_date.getMonth();
+    console.log(`month: ${month}`);
+
+    var date = end_date.getDate();
+    console.log(`date: ${date}`);
+
+    console.log(year);
+    query.created_at = {
+      $lt: new Date(year, month, date),
     }
-}, function (err, post) {
+    delete query.end_date;
+  }
+  console.log(query);
+
+  //if sort -- to be implemented
+
+  Sighting.find(query, function (err, post) {
     if (err) return next(err);
     res.json(post);
   });
